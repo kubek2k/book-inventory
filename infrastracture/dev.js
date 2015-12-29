937a80aaedec97b85c432080a703f4ca9882c586
Jakub Janczak <jakub.janczak@schibsted.pl>
2015-12-29 11:06:33 +0100

    Infrastructure code

---
+++ infrastracture/dev.js
@@ -0,0 +1,16 @@
+const heroin = require('heroin-js')
+
+const configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
+
+configurator({ name: 'another-book-inventory-dev',
+      region: 'us',
+      maintenance: false,
+      stack: 'cedar-14',
+      collaborators: 
+       [ 'plan3-labs@herokumanager.com',
+         'michal.kucharzyk@schibsted.pl',
+         'wojciech.kabala@schibsted.pl',
+         'jakub@plan3.se' ],
+      formation: [ { process: 'web', quantity: 1, size: 'Free' } ],
+      log_drains: [],
+      domains: [ 'another-book-inventory-dev.herokuapp.com' ] })
---
+++ infrastracture/heroku.js
@@ -0,0 +1,3 @@
+const heroin = require('heroin-js')
+
+const configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
